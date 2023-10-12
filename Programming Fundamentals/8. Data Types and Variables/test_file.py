c5c0874531a6f2747d7d6b43f2860dc67a7935c2
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-12 13:57:31 +0300

    updated

--- Programming Fundamentals/8. Data Types and Variables/test_file.py
+++ Programming Fundamentals/8. Data Types and Variables/test_file.py
@@ -3,6 +3,6 @@ name = str(input())
 for index, letter in enumerate(name):
     for index_2 in range(index + 1):
 
-        letter = "".join([letter * 2])
-        print(letter, end="")
+        letter = "".join([letter * 3])
+        print(letter)
 
