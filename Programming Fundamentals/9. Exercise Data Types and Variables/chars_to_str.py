8814ade9f0149012c43c219b50f984862176fce3
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-12 17:17:01 +0300

    updated

---
+++ Programming Fundamentals/9. Exercise Data Types and Variables/chars_to_str.py
@@ -0,0 +1,10 @@
+input_1 = int(input())
+input_2 = int(input())
+input_3 = int(input())
+input_4 = int(input())
+
+second = input_1 + input_2
+third = second / input_3
+fourth = int(third) * input_4
+
+print(fourth)
