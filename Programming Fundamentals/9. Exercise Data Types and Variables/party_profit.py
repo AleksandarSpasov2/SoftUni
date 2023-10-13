63673086b548b4a5ea12cdf669f89121988bc0f3
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-13 06:46:44 +0300

    updated excersices

--- Programming Fundamentals/9. Exercise Data Types and Variables/party_profit.py
+++ Programming Fundamentals/9. Exercise Data Types and Variables/party_profit.py
@@ -6,8 +6,6 @@ days = int(input())
 coins = 0
 
 for day in range(1, days + 1):
-    coins += 50
-    coins -= group_size * 2
 
     if day % 10 == 0:
         group_size -= 2
@@ -22,6 +20,9 @@ for day in range(1, days + 1):
         coins += 20 * group_size
         if day % 3 == 0:
             coins -= 2 * group_size
+
+    coins += 50
+    coins -= group_size * 2
 
 coins_per_person = floor(coins / group_size)
 
