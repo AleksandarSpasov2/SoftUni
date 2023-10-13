9ef5274007d120bfd644c9af7e559964fc9064d9
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-13 16:06:44 +0300

    update

---
+++ Programming Fundamentals/9. Exercise Data Types and Variables/More Exercises/exchange_integers.py
@@ -0,0 +1,12 @@
+user_input_2 = int(input())
+user_input_1 = int(input())
+
+a = user_input_2
+b = user_input_1
+
+user_input_1 = a
+user_input_2 = b
+
+print(f'Before:\na = {user_input_1}\nb = {user_input_2}\nAfter:\na = {user_input_2}\nb = {user_input_1}')
+
+
