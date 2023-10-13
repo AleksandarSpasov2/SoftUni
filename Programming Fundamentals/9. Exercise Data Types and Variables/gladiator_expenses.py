22b1d99acce794cd35491a7eb6bbd1efe6ba6d92
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-13 04:51:01 +0300

    Create gladiator_expenses.py

---
+++ Programming Fundamentals/9. Exercise Data Types and Variables/gladiator_expenses.py
@@ -0,0 +1,27 @@
+lost_games_count = int(input())
+
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+
+counter = 0
+total_repairs = 0
+
+for games in range(1, lost_games_count + 1):
+
+    if games % 2 == 0:
+        total_repairs += helmet_price
+
+    if games % 3 == 0:
+        total_repairs += sword_price
+
+    if games % 6 == 0:
+        counter += 1
+        total_repairs += shield_price
+
+    if counter == 12:
+        counter = 0
+        total_repairs += armor_price
+
+print(f'Gladiator expenses: {total_repairs:.2f} aureus')
