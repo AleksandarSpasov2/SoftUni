15caf09eefd4424ff67b49b509a3cba8a4bfafd7
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-14 04:40:08 +0300

    Update

--- Programming Fundamentals/9. Exercise Data Types and Variables/More Exercises/prime_number_checker.py
+++ Programming Fundamentals/9. Exercise Data Types and Variables/More Exercises/prime_number_checker.py
@@ -1,1 +1,17 @@
- number.")
+num = int(input())
+
+if num <= 0:
+    is_prime = False
+
+else:
+    is_prime = True
+
+    for i in range(2,num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+if is_prime:
+    print("Yes")
+else:
+    print("NO")
