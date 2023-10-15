5b61cc0e6ab0cff7bc20d7d8edd20679594152fb
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-15 18:51:55 +0300

    updated starnge_zoo

--- Programming Fundamentals/11. List Basics/strange_zoo.py
+++ Programming Fundamentals/11. List Basics/strange_zoo.py
@@ -1,8 +1,8 @@
-tail = str(input())
-body = str(input())
-head = str(input())
+my_list = []
 
-my_list = [tail, body, head]
+for i in range(3):
+    data_input = str(input())
+    my_list.append(data_input)
 
 my_list[0], my_list[2] = my_list[2], my_list[0]
 
