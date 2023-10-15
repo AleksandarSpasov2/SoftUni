584caccecc97a19d97aa0a919bfa17ab7bdfe992
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-15 22:06:07 +0300

    final update

--- Programming Fundamentals/11. List Basics/list_statistics.py
+++ Programming Fundamentals/11. List Basics/list_statistics.py
@@ -6,12 +6,12 @@ negative_list = []
 for i in range(n):
     num = int(input())
 
-    if num < 0:
-        negative_list.append(num)
-    else:
+    if num >= 0:
         positive_list.append(num)
+    else:
+        negative_list.append(num)
 
 print(positive_list)
 print(negative_list)
-print(positive_list.count())
-print(negative_list.count())
+print(f'Count of positives:', len(positive_list))
+print(f'Sum of negatives:', sum(negative_list))
