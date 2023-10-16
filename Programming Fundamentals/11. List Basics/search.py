dc5a4315ac53d9ea8d0e353359031baea208c8cf
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-16 07:19:55 +0300

    final 2.0

--- Programming Fundamentals/11. List Basics/search.py
+++ Programming Fundamentals/11. List Basics/search.py
@@ -1,18 +1,19 @@
 n = int(input())
 
-word = str(input())
+magic_word = str(input())
 
 my_list = []
 
 for i in range(n):
-    user_input = str(input())
-    my_list.append(user_input)
+    string = str(input())
+    my_list.append(string)
 
 print(my_list)
 
 filtered_string = []
 
-for current_string in my_list:
-    if word in current_string:
-        filtered_string.append(current_string)
+for j in my_list:
+    if magic_word in j:
+        filtered_string.append(j)
 print(filtered_string)
+
