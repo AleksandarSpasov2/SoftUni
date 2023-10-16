0de9f95d07ee45c263fadb78ca9cd05ba3e63034
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-16 07:33:37 +0300

    5.1 upload

---
+++ Programming Fundamentals/11. List Basics/numbers_filter.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+big_list = []
+
+for i in range(n):
+    user_input = int(input())
+    big_list.append(user_input)
+
+filtered_list = []
+command = str(input())
+
+if command == "even":
+    for j in big_list:
+        if j % 2 == 0:
+            filtered_list.append(j)
+
+elif command == "odd":
+    for j in big_list:
+        if j % 2 != 0:
+            filtered_list.append(j)
+
+if command == "negative":
+    for j in big_list:
+        if j < 0:
+            filtered_list.append(j)
+
+if command == "positive":
+    for j in big_list:
+        if j > 0:
+            filtered_list.append(j)
+
+print(filtered_list)
