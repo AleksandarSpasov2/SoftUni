680e623bd4f4c784e405185f8b63889d8c981e3b
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-20 02:47:58 +0300

    test4_updated

--- Programming Fundamentals/12. List Basics - Excercise/test.py
+++ Programming Fundamentals/12. List Basics - Excercise/test.py
@@ -1,22 +1,8 @@
 user_input = input().split()
 
-my_list = []
+mid_list = len(user_input) // 2
 
-for digit in user_input:
-    my_list.append(digit)
+left_side = user_input[:mid_list]
+right_side = user_input[mid_list:]
+transformed_list = []
 
-new_list_int = []
-
-for index in my_list:
-    new_list_int.append(int(index))
-
-newest_list = []
-
-for index_2 in new_list_int:
-    new_index = index_2 * 2
-    newest_list.append(new_index)
-
-for index_2 in newest_list:
-    
-
-print(newest_list)
