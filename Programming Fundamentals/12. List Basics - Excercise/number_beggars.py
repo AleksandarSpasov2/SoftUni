5fbe50edcb0b2003bf2f3c410fb16d7623b11553
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-19 18:03:49 +0300

    final update

--- Programming Fundamentals/12. List Basics - Excercise/number_beggars.py
+++ Programming Fundamentals/12. List Basics - Excercise/number_beggars.py
@@ -1,3 +1,21 @@
 first_input = input().split(", ")
 second_input = int(input())
 
+money_list = []
+
+for current_money in first_input:
+    money_list.append(int(current_money))
+
+final_sums = []
+
+star_index = 0
+
+while star_index < second_input:
+    current_beggar_sum = 0
+    for current_index in range(star_index, len(money_list), second_input):
+        current_beggar_sum += money_list[current_index]
+    final_sums.append(current_beggar_sum)
+    star_index += 1
+
+print(final_sums)
+
