a4ffb97a0d5ff674c96ef6aadda607b7928e77bf
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-20 05:42:32 +0300

    5.5 done

---
+++ Programming Fundamentals/11. List Basics/More Exercises - Outsource/5.5.py
@@ -0,0 +1,21 @@
+def list_string(list_input):
+    new_list = []
+    for index in list_input:
+        new_list.append(str(index))
+    return new_list
+
+
+def match_words(words):
+    magic_word = 0
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            magic_word += 1
+    return magic_word
+
+
+user_input = [1, 2, "aba", "b"]
+
+user_input_1 = list_string(user_input)
+user_input_2 = match_words(user_input_1)
+print(user_input_2)
+print(user_input_1)
