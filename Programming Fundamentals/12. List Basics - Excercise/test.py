964e3293e306e0be62b79da4e995152499e16cce
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-20 03:08:18 +0300

    test5_update

--- Programming Fundamentals/12. List Basics - Excercise/test.py
+++ Programming Fundamentals/12. List Basics - Excercise/test.py
@@ -6,3 +6,10 @@ left_side = user_input[:mid_list]
 right_side = user_input[mid_list:]
 transformed_list = []
 
+for card in range(len(left_side)):
+    transformed_list.append(left_side[card])
+    transformed_list.append(right_side[card])
+
+print(transformed_list)
+
+
