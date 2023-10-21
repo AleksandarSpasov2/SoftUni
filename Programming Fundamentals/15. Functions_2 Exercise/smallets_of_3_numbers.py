c002c96debb6103609c3e67ece48982a8b07f993
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-22 02:24:44 +0300

    15.1 done

---
+++ Programming Fundamentals/15. Functions_2 Exercise/smallets_of_3_numbers.py
@@ -0,0 +1,11 @@
+def find_smallest(num_1, num_2, num_3):
+    smallest = min(num_1, num_2, num_3)
+
+    return print(smallest)
+
+
+number_1 = int(input())
+number_2 = int(input())
+number3 = int(input())
+
+find_smallest(number_1, number_2, number3)
