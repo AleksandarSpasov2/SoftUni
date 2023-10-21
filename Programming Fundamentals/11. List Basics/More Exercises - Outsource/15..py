b27f852db7782b18fcf39ca09f8cf8bd3e998469
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-21 03:53:06 +0300

    exercise 15 done

---
+++ Programming Fundamentals/11. List Basics/More Exercises - Outsource/15..py
@@ -0,0 +1,10 @@
+from random import shuffle
+
+
+def shuffle_list(list_1):
+    shuffle(list_1)
+    return print(list_1)
+
+
+my_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+shuffle_list(my_list)
