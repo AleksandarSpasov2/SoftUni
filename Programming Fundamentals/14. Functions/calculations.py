87f66303440cef1f29a49def52fb0fe3b5ccdaa5
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-21 19:51:57 +0300

    exercise 3 done

---
+++ Programming Fundamentals/14. Functions/calculations.py
@@ -0,0 +1,19 @@
+def calculation_function(action: str, num_1: int, num_2: int):
+    if action == "multiply":
+        result = num_1 * num_2
+        return print(result)
+    elif action == "divide":
+        result = num_1 / num_2
+        return print(result)
+    elif action == "add":
+        result = num_1 + num_2
+        return print(result)
+    elif action == "subtract":
+        result = num_1 - num_2
+        return print(result)
+
+
+action = str(input())
+num_1_input = int(input())
+num_2_input = int(input())
+calculation_function(action, num_1_input, num_2_input)
