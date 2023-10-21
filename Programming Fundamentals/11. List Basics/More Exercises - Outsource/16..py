ec28d527e16bcbad20bf250518444c7452cfd8d2
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-21 04:04:25 +0300

    exercise 16 done

---
+++ Programming Fundamentals/11. List Basics/More Exercises - Outsource/16..py
@@ -0,0 +1,9 @@
+def first_last_elements(list_1):
+    new_list = list_1[:5] + list_1[-5:]
+
+    return print(new_list)
+
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+first_last_elements(my_list)
