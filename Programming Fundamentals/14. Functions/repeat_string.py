5df197f1680dc139db3ecd18c204208c14fe9e3e
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-21 19:55:41 +0300

    exercise 4 done

---
+++ Programming Fundamentals/14. Functions/repeat_string.py
@@ -0,0 +1,8 @@
+def multiply_string(str_input, counter):
+    result_string = str_input * counter
+    return print(result_string)
+
+
+user_input_str = input()
+counter_input = int(input())
+multiply_string(user_input_str, counter_input)
