a6809905b25f789a02e076040ccf6948af3ab9e6
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-21 20:06:21 +0300

    exercise 6 done

---
+++ Programming Fundamentals/14. Functions/calculate_rectange_area.py
@@ -0,0 +1,8 @@
+def rectangle_area(width, height):
+    result = (width * height)
+    return print(result)
+
+
+width_input = int(input())
+height_input = int(input())
+rectangle_area(width_input, height_input)
