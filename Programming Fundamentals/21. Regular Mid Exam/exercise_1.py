1559955a42fccf738da516a03d77a40c8c359c31
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-22 15:05:47 +0300

    exam started

---
+++ Programming Fundamentals/21. Regular Mid Exam/exercise_1.py
@@ -0,0 +1,7 @@
+budget = float(input())
+number_of_student = int(input())
+flour_price = float(input())
+egg_price = float(input())
+apron_price = float(input())
+
+student_package = flour_price + (egg_price * 10) + apron_price
