04e1567a73b23f5d5be3ceaebe80e4cd5ac45776
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-23 03:42:59 +0300

    Functions extra1 done

---
+++ Programming Fundamentals/Extra Exercises/Functions/1.py
@@ -0,0 +1,11 @@
+def maximum_number(number_1, number_2, number_3):
+    max_result = max(number_1, number_2, number_3)
+    return max_result
+
+
+n_1 = 10
+n_2 = 12
+n_3 = 15
+
+max_1 = maximum_number(n_1, n_2, n_3)
+print(max_1)
