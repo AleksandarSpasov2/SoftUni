094d9e08e857040e1f80367b6cea1de0e607d3f6
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-24 03:47:19 +0300

    15.1 remake

---
+++ Programming Fundamentals/15. Functions Exercise/exercises_redo/1.py
@@ -0,0 +1,11 @@
+def smallest(some_list: list):
+    return min(some_list)
+
+
+first = int(input())
+second = int(input())
+third = int(input())
+
+small_element = smallest([first, second, third])
+
+print(small_element)
