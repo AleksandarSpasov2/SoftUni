150617d070c9ce0c09e5c8f5bb6782f1afb11076
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-27 14:13:53 +0300

    exercise 1 done

---
+++ Programming Fundamentals/18 Lits Advanced Exercise/which_are_in.py
@@ -0,0 +1,12 @@
+first_list = input().split()
+second_list = input().split()
+
+substrings = []
+
+for element_string_1 in first_list:
+    for element_string_2 in second_list:
+        if element_string_1 in element_string_2:
+            substrings.append(element_string_1)
+            break
+
+print(substrings)
