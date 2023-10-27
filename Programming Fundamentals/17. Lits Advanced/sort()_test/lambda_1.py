4e573fd49a4046f2a1a27d0d8fdcfdc5f8dcd07a
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-27 05:15:34 +0300

    Update lambda_1.py

--- Programming Fundamentals/17. Lits Advanced/sort()_test/lambda_1.py
+++ Programming Fundamentals/17. Lits Advanced/sort()_test/lambda_1.py
@@ -3,3 +3,5 @@ my_list = ["a-10", "b-2", "c-5"]
 
 sorted_list = sorted(my_list, key=lambda x: int(x.split("-")[1]))
 
+new_list = [element.split("-")[0] for element in sorted_list]
+print(new_list)
