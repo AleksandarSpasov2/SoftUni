310f737d3adf9ce5b238cfdf3e46a2ef6b5afe1c
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-29 03:17:25 +0200

    update

--- Programming Fundamentals/18 Lits Advanced Exercise/office_chairs.py
+++ Programming Fundamentals/18 Lits Advanced Exercise/office_chairs.py
@@ -2,20 +2,18 @@ rooms = int(input())
 
 extra_chairs = 0
 for current_room in range(1, rooms + 1):
-    chairs, visitors = input().split()
+    room_info = input().split()
+    chairs = room_info[0]
+    visitors = int(room_info[1])
 
-    if len(chairs) < int(visitors):
-        needed_chairs = abs(len(chairs) - int(visitors))
-        extra_chairs -= needed_chairs
+    available_chairs = chairs.count('X')
+
+    if available_chairs < visitors:
+        needed_chairs = visitors - available_chairs
         print(f'{needed_chairs} more chairs needed in room {current_room}')
     else:
-        needed_chairs = abs(len(chairs) - int(visitors))
-        extra_chairs += needed_chairs
+        free_chairs = available_chairs - visitors
+        extra_chairs += free_chairs
 
 if extra_chairs > 0:
     print(f'Game On, {extra_chairs} free chairs left')
-
-
-
-
-
--- Programming Fundamentals/18 Lits Advanced Exercise/office_chairs_2.0.py
+++ Programming Fundamentals/18 Lits Advanced Exercise/office_chairs_2.0.py
@@ -1,16 +1,22 @@
-def extra_chairs(string_input):
+
+def e_chairs(int_input):
     extra_chairs = 0
-    for current_room in range(1, string_input):
-        chairs, visitors = input().split()
-        if len(chairs) < int(visitors):
-            needed_chairs = abs(len(chairs) - int(visitors))
-            extra_chairs -= needed_chairs
+    for current_room in range(1, rooms + 1):
+        room_info = input().split()
+        chairs = room_info[0]
+        visitors = int(room_info[1])
+
+        available_chairs = chairs.count('X')
+
+        if available_chairs < visitors:
+            needed_chairs = visitors - available_chairs
             return f'{needed_chairs} more chairs needed in room {current_room}'
         else:
-            needed_chairs = abs(len(chairs) - int(visitors))
-            extra_chairs += needed_chairs
-            return f'Game On, {extra_chairs} free chairs left'
+            free_chairs = available_chairs - visitors
+            extra_chairs += free_chairs
+
+    return f'Game On, {extra_chairs} free chairs left'
 
 
 rooms = int(input())
-print(extra_chairs(rooms))
+print(e_chairs(rooms))
