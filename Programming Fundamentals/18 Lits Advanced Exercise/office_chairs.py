9138258a71116da33d852335ab7111605ce7735c
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-29 03:19:54 +0300

    first version of exercise 5 upload

--- Programming Fundamentals/18 Lits Advanced Exercise/office_chairs.py
+++ Programming Fundamentals/18 Lits Advanced Exercise/office_chairs.py
@@ -1,6 +1,21 @@
 rooms = int(input())
 
-for _ in range(rooms):
-    chairs = int(input().split().int(input())
-                 
+extra_chairs = 0
+for current_room in range(1, rooms + 1):
+    chairs, visitors = input().split()
+
+    if len(chairs) < int(visitors):
+        needed_chairs = abs(len(chairs) - int(visitors))
+        extra_chairs -= needed_chairs
+        print(f'{needed_chairs} more chairs needed in room {current_room}')
+    else:
+        needed_chairs = abs(len(chairs) - int(visitors))
+        extra_chairs += needed_chairs
+
+if extra_chairs > 0:
+    print(f'Game On, {extra_chairs} free chairs left')
+
+
+
+
 
