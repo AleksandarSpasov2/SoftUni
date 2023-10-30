9d9cb7c8f8595aa2b85ea163714599357c6ee873
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-10-30 12:36:18 +0200

    Create black_falg.py

---
+++ Programming Fundamentals/19. Exam preparation/black_falg.py
@@ -0,0 +1,22 @@
+days_of_plunder = int(input())
+daily_plunder = int(input())
+expected_plunder = float(input())
+
+plunder = 0
+
+for day in range(1, days_of_plunder + 1):
+    plunder += daily_plunder
+
+    if day % 3 == 0:
+        plunder += 0.5 * daily_plunder
+
+    if day % 5 == 0:
+        plunder -= 0.3 * plunder
+
+if plunder >= expected_plunder:
+    print(f'Ahoy! {plunder:.2f} plunder gained.')
+else:
+    difference = plunder / expected_plunder * 100
+    print(f'Collected only {difference:.2f}% of the plunder.')
+
+
