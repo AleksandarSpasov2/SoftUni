f102a425f284d4f13a27187794c19e326cea4684
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-01 21:49:16 +0200

    Update man_o_war.py

---
+++ Programming Fundamentals/19. Exam preparation/man_o_war.py
@@ -0,0 +1,13 @@
+def main():
+    status_pirate = input().split(">")
+    status_warship = input().split(">")
+    max_health = int(input())
+
+    while True:
+        command = input()
+        if command == "Retire":
+            break
+        command_input = command.split()
+        commad
+
+
