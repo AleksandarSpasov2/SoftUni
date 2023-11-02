6a87444e6cd7a9563b804a46e4b495f2c18ffbd7
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-02 05:00:55 +0200

    updates

--- Programming Fundamentals/19. Exam preparation/man_o_war.py
+++ Programming Fundamentals/19. Exam preparation/man_o_war.py
@@ -8,6 +8,34 @@ def main():
         if command == "Retire":
             break
         command_input = command.split()
-        commad
+        action = command_input[1]
+
+        if action == "Fire":
+            fire_function(status_warship, command_input)
+
+        elif action == "Defend":
+
+
+
+
+
+def fire_function(status_warship, command_input):
+    index = command_input[1]
+    damage = command_input[2]
+    if index in status_warship:
+        status_warship[index] -= damage
+        if status_warship[index] == 0:
+            print("You won! The enemy ship has sunken.")
+
+
+def defend_function(command_input, status_pirate):
+    start_index = command_input[1]
+    end_index = command_input[2]
+    damage = command_input[3]
+    if start_index in status_pirate and end_index in status_pirate:
+
+
+
+
 
 
