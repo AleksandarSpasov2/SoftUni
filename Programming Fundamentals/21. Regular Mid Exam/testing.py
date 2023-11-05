f1bcc9aead2206cb36051b25b889818ccc0df47d
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-05 19:11:28 +0200

    Update testing.py

--- Programming Fundamentals/21. Regular Mid Exam/testing.py
+++ Programming Fundamentals/21. Regular Mid Exam/testing.py
@@ -1,1 +1,6 @@
-def even_test(list_of_num: list): -> list
+def even_test(list_of_num: list):
+    filtered_list = [num for num in list_of_num if num % 2 != 0]
+    return  filtered_list
+
+
+even_test()
