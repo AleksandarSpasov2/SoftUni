a53de41e8054e1b4706fdbf16d51acc6ee1c37bd
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-06 16:48:58 +0200

    Update black_falg.py

--- Programming Fundamentals/19. Exam preparation/black_falg.py
+++ Programming Fundamentals/19. Exam preparation/black_falg.py
@@ -15,6 +15,7 @@ for day in range(1, days_of_plunder + 1):
 
 if plunder >= expected_plunder:
     print(f'Ahoy! {plunder:.2f} plunder gained.')
+    
 else:
     difference = plunder / expected_plunder * 100
     print(f'Collected only {difference:.2f}% of the plunder.')
