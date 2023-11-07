365f57a1df53cea32b8da5067074d92e47edce28
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-08 01:41:34 +0200

    Create email.py

---
+++ Programming Fundamentals/22. Obejct and Clases/email.py
@@ -0,0 +1,14 @@
+class Email:
+    def __init__(self, sender, receiver, content):
+        self.sender = sender
+        self.receiver = receiver
+        self.content = content
+
+    def sent(self):
+        is_sent = True
+
+
+    def get_info(self):
+        return f'{}'
+
+
