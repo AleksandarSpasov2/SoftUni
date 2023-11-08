237bd5570784c3eee984cde18072798aefa8a95b
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-08 10:19:30 +0200

    Update email.py

--- Programming Fundamentals/22. Obejct and Clases/email.py
+++ Programming Fundamentals/22. Obejct and Clases/email.py
@@ -10,7 +10,23 @@ class Email:
 
     def get_info(self):
         return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
-    
+
+
+final_emails = []
+command = input()
+while command != "Stop":
+    sender, receiver, content = command.split()
+    email_info = Email(sender, receiver, content)
+    final_emails.append(email_info)
+    command = input()
+
+indexes = [int(s) for s in input().split(", ")]
+for index in indexes:
+    final_emails[index].send()
+
+for current_email in final_emails:
+    print(current_email.get_info())
+
 
 
 
