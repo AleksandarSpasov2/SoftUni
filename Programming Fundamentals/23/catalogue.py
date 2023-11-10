a547e0b0f097853064337de33616a9a04365373e
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-10 02:35:57 +0200

    Update catalogue.py

--- Programming Fundamentals/23/catalogue.py
+++ Programming Fundamentals/23/catalogue.py
@@ -1,2 +1,18 @@
 class Catalogue:
-    
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product_name: str):
+        self.products.append(product_name)
+
+    def get_by_letter(self, first_letter: str):
+        first_letter_list = []
+        for word in self.products:
+            if word[0] == first_letter_list:
+                first_letter_list.append(word)
+        return first_letter_list
+
+    def __repr__(self):
+        return f'Items in the {self.name} catalogue:\n '
+
