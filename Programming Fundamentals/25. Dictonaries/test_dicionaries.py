022020d376858fc687a21726ec01d903c2c8f5e3
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-11 17:38:01 +0200

    Update test_dicionaries.py

---
+++ Programming Fundamentals/25. Dictonaries/test_dicionaries.py
@@ -0,0 +1,12 @@
+keys = ['name', "age", 'title']
+
+values = ['Ivan', '28', 'Computer Science']
+
+my_dic = {}
+
+for index in range(len(keys)):
+    key = keys[index]
+    value = values[index]
+    my_dic[key] = value
+
+print(my_dic)
