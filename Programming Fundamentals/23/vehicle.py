afe6857947fc88afbaba4c7a89308d441079a238
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-11 15:50:53 +0200

    updates

--- Programming Fundamentals/23/vehicle.py
+++ Programming Fundamentals/23/vehicle.py
@@ -1,30 +1,31 @@
 class Vehicle:
-    owner = None
 
     def __init__(self, type, model, price):
         self.type = type
         self.model = model
         self.price = price
+        self.owner = None
 
     def buy(self, money: int, owner: str):
-        if money >= self.price and Vehicle.owner is None:
-            Vehicle.owner = owner
-            return f"Successfully bought a {self.type}. Change: {owner}"
+        if money >= self.price and self.owner is None:
+            self.owner = owner
+            change = money - self.price
+            return f"Successfully bought a {self.type}. Change: {change}"
 
         elif money < self.price:
             return f"Sorry, not enough money"
-        elif Vehicle.owner is not None:
+        elif self.owner is not None:
             return f"Car already sold"
 
     def sell(self):
-        if Vehicle.owner is not None:
-            Vehicle.owner = None
+        if self.owner is not None:
+            self.owner = None
         else:
             return f"Vehicle has no owner"
 
     def __repr__(self):
-        if Vehicle.owner is not None:
-            return f"{self.model} {self.type} is owned by: {Vehicle.owner}"
+        if self.owner is not None:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
         else:
             return f"{self.model} {self.type} is on sale: {self.price}"
 
