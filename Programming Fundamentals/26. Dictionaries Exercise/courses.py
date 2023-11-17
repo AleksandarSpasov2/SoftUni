f3377916d39f516e5c25cb08acbe4d7090872fd0
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-17 06:46:15 +0200

    Update courses.py

--- Programming Fundamentals/26. Dictionaries Exercise/courses.py
+++ Programming Fundamentals/26. Dictionaries Exercise/courses.py
@@ -7,18 +7,18 @@ def main():
             break
         name, student = command.split(":")
         if name not in course_dict.keys():
-            course_dict[name] = student
+            course_dict[name] = [student]
         else:
-            course_dict[name] += student
+            course_dict[name].append(student)
 
     print_dict(course_dict)
 
 
 def print_dict(course_dict):
     for key, value in course_dict.items():
-        print(f'{key}:{len(course_dict[key])}')
-
-        print(f'"\n".join(--{value})')
+        print(f'{key}:{len(value)}')
+        for student in value:
+            print(f'--{student}')
 
 
 main()
