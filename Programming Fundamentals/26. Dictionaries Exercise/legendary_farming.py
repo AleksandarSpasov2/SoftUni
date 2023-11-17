8f69a87cdc31828582cd12e53d36e85e19166fa1
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-17 21:16:44 +0200

    Update legendary_farming.py

--- Programming Fundamentals/26. Dictionaries Exercise/legendary_farming.py
+++ Programming Fundamentals/26. Dictionaries Exercise/legendary_farming.py
@@ -8,5 +8,14 @@ while not obtained:
     for index in range(0, len(items_input), 2):
         value = int(items_input[index])
         material = items_input[index + 1].lower()
+        if material not in items_dict.keys():
+            items_dict[material] = 0
+        items_dict[material] += value
+
+        if items_dict["shards"] >= 250:
+            items_dict["shards"] -= 250
+            print("Shadowmourne obtained!")
+            obtained = True
+        elif:
     items_input = input().split()
 
