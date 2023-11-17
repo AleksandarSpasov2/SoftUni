0cf8d5f2ba2f6a030a18ce93ff9421df87fd80f6
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-17 06:32:18 +0200

    Create courses.py

---
+++ Programming Fundamentals/26. Dictionaries Exercise/courses.py
@@ -0,0 +1,25 @@
+def main():
+    course_dict = {}
+    while True:
+
+        command = input()
+        if command == "end":
+            break
+        name, student = command.split(":")
+        if name not in course_dict.keys():
+            course_dict[name] = student
+        else:
+            course_dict[name] += student
+
+    print_dict(course_dict)
+
+
+def print_dict(course_dict):
+    for key, value in course_dict.items():
+        print(f'{key}:{len(course_dict[key])}')
+
+        print(f'"\n".join(--{value})')
+
+
+main()
+
