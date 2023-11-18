e20dfe26e30c206db8dbfa8c1c4b990edb4a20c1
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-18 06:16:48 +0200

    Update force_book.py

--- Programming Fundamentals/26. Dictionaries Exercise/force_book.py
+++ Programming Fundamentals/26. Dictionaries Exercise/force_book.py
@@ -1,7 +1,7 @@
 def main():
-    user_input = input()
     unique_force_user = {}
     while True:
+        user_input = input()
         if user_input == "Lumpawaroo":
             break
 
@@ -11,21 +11,43 @@ def main():
 
         elif "->" in user_input:
             force_user, force_side = user_input.split(" -> ")
+            user_function(unique_force_user, force_side, force_user)
+
+    print_function(unique_force_user)
 
 
 def force_function(unique_force_user, force_side, force_user):
-    if force_side not in unique_force_user.keys() and force_user not in unique_force_user.values():
-        unique_force_user[force_side] = [force_user]
-    elif force_user not in unique_force_user.values():
-        unique_force_user[force_side] += [force_user]
-    elif force_user in unique_force_user.values():
-        pass
+    if force_side not in unique_force_user.keys():
+        unique_force_user[force_side] = []
+    user_exists = False
+    for user in unique_force_user.values():
+        if force_user in user:
+            user_exists = True
+            break
+    if not user_exists:
+        unique_force_user[force_side].append(force_user)
 
 
 def user_function(unique_force_user, force_side, force_user):
-    if
+    for side, users in unique_force_user.items():
+        if force_user in users:
+            users.remove(force_user)
+            break
+        if force_side not in unique_force_user:
+            unique_force_user[force_side] = []
+        unique_force_user[force_side].append(force_user)
+
+        print(f"{force_user} joins the {force_side} side!")
 
 
+def print_function(unique_force_user):
+    for force_side, force_users_list in unique_force_user.items():
+        if force_users_list:
+            print(f"Side: {force_side}, Members: {len(force_users_list)}")
+            for user in force_users_list:
+                print(f"! {user}")
+
+main()
 
 
 
