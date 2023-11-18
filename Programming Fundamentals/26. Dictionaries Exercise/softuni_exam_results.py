41a4916ea703c0d53587c8f98b1025eb6c49cd3e
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-18 13:37:52 +0200

    updates

---
+++ Programming Fundamentals/26. Dictionaries Exercise/softuni_exam_results.py
@@ -0,0 +1,38 @@
+def main():
+    students = {}
+    command = input()
+
+    while command != "exam finished":
+        command_info = command.split("-")
+        username = command_info[0]
+
+        if command_info[1] == "banned":
+            if username in students.keys():
+                del students[username]
+                continue #-------------------------------------
+
+        language = command_info[1]
+        points = int(command_info[2])
+
+        if username not in students.keys():
+            students[username] = []
+        students[username].append(language)
+        students[username].append(points)
+
+
+
+def print_function(students):
+    for key, value in students.items():
+        print(f'{key} | {value[1]}')
+
+    
+
+
+
+
+
+
+
+
+
+
