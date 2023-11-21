86e3d6543c83667b1a885bbfa30d92a56a38ab0b
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-21 21:12:45 +0200

    Update test.py

--- Programming Fundamentals/28. Text Processing/test.py
+++ Programming Fundamentals/28. Text Processing/test.py
@@ -2,8 +2,12 @@ def reverse_text(text: str):
     reversed_text = text[::-1]
     return reversed_text
 
+
 def print_half_text(text: str):
-    
+    length = len(text) / 2
+    half_text = text[length::]
+    return half_text
+
 
 text_name = 'abcd'
 
@@ -13,3 +17,4 @@ print(new_text)
 
 print(reverse_text(text_name))
 
+print(print_half_text(text_name))
