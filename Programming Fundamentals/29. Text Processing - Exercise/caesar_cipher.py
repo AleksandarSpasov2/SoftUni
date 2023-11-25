46b096c130a134374fa6c3d885582642d61faa6d
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-25 02:42:07 +0200

    updates

--- Programming Fundamentals/29. Text Processing - Exercise/caesar_cipher.py
+++ Programming Fundamentals/29. Text Processing - Exercise/caesar_cipher.py
@@ -1,2 +1,9 @@
 def main():
-    
+    message_input = input()
+    new_message = ''
+    for character in message_input:
+        new_character = chr(ord(character) + 3)
+        new_message += new_character
+    print(new_message)
+
+main()
