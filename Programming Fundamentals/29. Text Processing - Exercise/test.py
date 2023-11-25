b699a704f867086f463fa478a1cffbc0dce80390
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-25 07:17:39 +0200

    Update test.py

--- Programming Fundamentals/29. Text Processing - Exercise/test.py
+++ Programming Fundamentals/29. Text Processing - Exercise/test.py
@@ -1,5 +1,24 @@
-some_string = input().split()
+def generate_rage_message(input_str):
+    unique_symbols = set()
+    rage_message = ""
+    i = 0
 
-for element in some_string:
-    if element != "\\":
-        print(element)
+    while i < len(input_str):
+        symbol = input_str[i].upper()
+        unique_symbols.add(symbol)
+        i += 1
+
+        count = 0
+        while i < len(input_str) and input_str[i].isdigit():
+            count = count * 10 + int(input_str[i])
+            i += 1
+
+        rage_message += symbol * count
+
+    print(f"Unique symbols used: {len(unique_symbols)}")
+    print(rage_message)
+
+
+input_str = input()
+
+generate_rage_message(input_str)
