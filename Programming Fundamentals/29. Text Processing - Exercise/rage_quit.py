f5fca851ec61190dd3203a41b11b7499de5bee7f
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-11-27 06:38:01 +0200

    Update rage_quit.py

--- Programming Fundamentals/29. Text Processing - Exercise/rage_quit.py
+++ Programming Fundamentals/29. Text Processing - Exercise/rage_quit.py
@@ -1,16 +1,21 @@
 def main():
     user_input = input()
     list_input = list_maker(user_input)
-    counter = counter_function(list_input)
+    unique_symbols_count = counter_function(list_input)
+    rage_msg = rage_message(list_input)
+
+    # Print the number of unique symbols
+    print(f"Unique symbols used: {unique_symbols_count}")
+
+    # Print the rage message
+    print(rage_msg)
 
 
 def list_maker(user_input):
     list_input = []
     for char in user_input:
-        if char.isdigit():
-            list_input.append(char)
-        else:
-            list_input.append(char)
+        if char.isalpha():  # Only append non-numeric characters
+            list_input.append(char.upper())
     return list_input
 
 
@@ -19,9 +24,17 @@ def counter_function(list_input):
     return len(unique_characters)
 
 
-def range_message(list_input):
+def rage_message(list_input):
     final_message = ''
     for index, value in enumerate(list_input):
         message = ''
         number = 0
-        if
+        if value.isdigit():
+            number = int(value)
+            message = list_input[index - 1] * number
+        final_message += message
+
+    return final_message
+
+
+main()
