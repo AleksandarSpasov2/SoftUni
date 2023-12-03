1e056037fdc8a79a5892839e8c1a1902b7567577
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-12-03 15:03:57 +0200

    Update messages_manager.py

--- Programming Fundamentals/38. Final Exam/messages_manager.py
+++ Programming Fundamentals/38. Final Exam/messages_manager.py
@@ -8,27 +8,32 @@ while command[0] != 'Statistics':
 
     if command[0] == 'Add':
         username, sent, received = command[1], int(command[2]), int(command[3])
-        if username not in users_dict.keys():
+        if username not in users_dict:
             users_dict[username] = {"sent": sent, 'received': received}
 
     elif command[0] == 'Message':
-        sender, reciever = command[1], command[2]
-        if sender in users_dict.keys() and reciever in users_dict.keys():
+        sender, receiver = command[1], command[2]
+        if sender in users_dict and receiver in users_dict:
             users_dict[sender]['sent'] += 1
-            users_dict[reciever]['received'] += 1
-        for key, value in users_dict.items():
-            if value['sent'] > 10 or value['received'] > 10:
-                del key
-                print(f'{sender} reached the capacity!')
+            users_dict[receiver]['received'] += 1
+
+            for user in [sender, receiver]:
+                if users_dict[user]['sent'] + users_dict[user]['received'] >= capacity:
+                    print(f'{user} reached the capacity!')
+                    del users_dict[user]
+                    break
 
     elif command[0] == 'Empty':
         username = command[1]
         if username == 'All':
             users_dict.clear()
         else:
-            if username in users_dict.keys():
+            if username in users_dict:
                 del users_dict[username]
-        
-
 
     command = input().split("=")
+
+print(f'Users count: {len(users_dict)}')
+for key, value in users_dict.items():
+    number_of_messages = value['sent'] + value['received']
+    print(f'{key} - {number_of_messages}')
