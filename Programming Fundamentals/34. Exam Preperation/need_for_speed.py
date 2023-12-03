4576a5f2e3b3e2d6e4772ccf0c200ab4e0e267b1
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-12-03 07:41:19 +0200

    Update need_for_speed.py

--- Programming Fundamentals/34. Exam Preperation/need_for_speed.py
+++ Programming Fundamentals/34. Exam Preperation/need_for_speed.py
@@ -7,3 +7,30 @@ for cars in range(number_of_cars):
     fuel = int(user_input[2])
     car_dict[car] = {'mileage': 0, "fuel": 0}
 
+command = input().split(':')
+while command[0] != 'Stop':
+    action = command[0]
+    if action == 'Drive':
+        car = command[1]
+        distance = int(command[2])
+        fuel = int(command[3])
+        if fuel >= car_dict[car]['fuel']:
+            car_dict[car]['fuel'] -= fuel
+            car_dict[car]['mileage'] += distance
+            print(f'{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.')
+        else:
+            print(f"Not enough fuel to make that ride")
+
+        for key, value in car_dict.items():
+            if value["mileage"] >= 100000:
+                del car_dict[key]
+
+    elif action == 'Refuel':
+        car = command[1]
+        fuel = int(command[2])
+        for key, value in car_dict.items():
+            
+
+    elif action == 'Revert':
+        car = command[1]
+        kilometers = int(command[2])
