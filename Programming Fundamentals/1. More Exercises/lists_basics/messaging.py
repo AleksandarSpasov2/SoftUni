8d270805a01f36849206b51a16986d1af59efd2f
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-12-06 07:26:02 +0200

    Create messaging.py

---
+++ Programming Fundamentals/1. More Exercises/lists_basics/messaging.py
@@ -0,0 +1,15 @@
+def sum_list(numbers_input):
+    numbers_input_sum = []
+    for num in numbers_input:
+        number = 0
+        for element in num:
+            number += element
+        numbers_input_sum.append(number)
+        number = 0
+
+
+def 
+
+numbers_input = list(map(int, input().split()))
+string_input = input()
+new_message = ''
