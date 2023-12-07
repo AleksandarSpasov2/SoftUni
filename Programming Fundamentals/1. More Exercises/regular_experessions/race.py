576a0e146e9a01676e3e72a73c3e27109f76cf59
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-12-07 09:56:41 +0200

    Update race.py

--- Programming Fundamentals/1. More Exercises/regular_experessions/race.py
+++ Programming Fundamentals/1. More Exercises/regular_experessions/race.py
@@ -5,5 +5,8 @@ participants = input().split(", ")
 info = input()
 
 while info != 'end of race':
-
+    match_name = ''.join(re.findall(r'[a-z]', info, re.IGNORECASE))
+    match_number = ''.join(re.findall(r'\d', info))
+    print(match_name)
+    print(match_number)
     info = input()
