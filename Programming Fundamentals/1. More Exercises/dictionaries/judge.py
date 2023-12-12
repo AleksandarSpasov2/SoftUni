598ad618630be8df1659e23fb30f6eea82044e35
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-12-12 18:22:25 +0200

    Update judge.py

--- Programming Fundamentals/1. More Exercises/dictionaries/judge.py
+++ Programming Fundamentals/1. More Exercises/dictionaries/judge.py
@@ -1,9 +1,14 @@
 user_input = input().split('->')
+main_dict = {}
+
 
 while True:
     if user_input[0] == 'no more time':
         break
 
     username, contest, points = user_input[0], user_input[1], int(user_input[2])
+    if username not in main_dict.keys():
+        main_dict[username] = ['contest': 0, "points": 0]
+        
 
     user_input = input().split('->')
