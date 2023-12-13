ae2aa55fd5574aa7498072cd9d41db24e775b1c2
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-12-13 06:27:20 +0200

    Update legendary_farming.py

--- Programming Fundamentals/1. More Exercises/dictionaries/legendary_farming.py
+++ Programming Fundamentals/1. More Exercises/dictionaries/legendary_farming.py
@@ -1,10 +1,36 @@
-main_dict = {
-    'shadowmourne': {'shards': 0}
-    'valanyr': {'fragments': 0}
-    'dragonwrath': {'motes': 0}
+items = {
+    'shards': 0,
+    'fragments': 0,
+    'motes': 0
 }
 
-user_input = input().split()
-for element in range(len(user_input), 2):
-    value = int(user_input[0])
-    material = user_input[1]
+obtained = False
+
+while not obtained:
+    items_input = input().split()
+
+    for element in range(0, len(items_input), 2):
+        quantity = int(items_input[element])
+        material = items_input[element + 1].lower()
+
+        if material not in items.keys():
+            items[material] = 0
+        items[material] += quantity
+
+        if items["shards"] >= 250:
+            items["shards"] -= 250
+            print("Shadowmourne obtained!")
+            obtained = True
+
+        elif items["fragments"] >= 250:
+            items["fragments"] -= 250
+            print("Valanyr obtained!")
+            obtained = True
+
+        elif items["motes"] >= 250:
+            items["motes"] -= 250
+            print("Dragonwrath obtained!")
+            obtained = True
+
+for key, value in items.items():
+    print(f'{key}: {value}')
