4016c59fee577f187fa5e57584cd3f274b11cbe5
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-12-13 05:59:35 +0200

    updates

---
+++ Programming Fundamentals/1. More Exercises/dictionaries/legendary_farming.py
@@ -0,0 +1,10 @@
+main_dict = {
+    'shadowmourne': {'shards': 0}
+    'valanyr': {'fragments': 0}
+    'dragonwrath': {'motes': 0}
+}
+
+user_input = input().split()
+for element in range(len(user_input), 2):
+    value = int(user_input[0])
+    material = user_input[1]
