6d86d22f6969f8117227beaf3d8a332fee6382a7
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2023-12-18 20:28:16 +0200

    updates

---
+++ Programming Fundamentals/1. More Exercises/text_processing/the_imitation_game.py
@@ -0,0 +1,18 @@
+def main():
+    encrypted_message = input()
+
+    command = input().split("|")
+
+    while command[0] != "Decode":
+
+        if command[0] == 'Move':
+
+
+        command = input().split("|")
+
+
+def move_function(command, encrypted_message):
+    number_of_letters = command[1]
+    first_letters = encrypted_message[:number_of_letters]
+    encrypted_message = encrypted_message[number_of_letters:] + first_letters
+    return encrypted_message
