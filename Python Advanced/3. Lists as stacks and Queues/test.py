327de456ed0b786fc93db5de563349fa95d505ce
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-10 21:36:00 +0200

    Create test.py

---
+++ Python Advanced/3. Lists as stacks and Queues/test.py
@@ -0,0 +1,21 @@
+def first():
+    print('a')
+
+
+def second():
+    print('b')
+
+
+def third():
+    print('c')
+
+
+def all_stacks():
+    first()
+    second()
+    third()
+
+
+a = [2, 5, 10]
+all_stacks()
+a = 5
