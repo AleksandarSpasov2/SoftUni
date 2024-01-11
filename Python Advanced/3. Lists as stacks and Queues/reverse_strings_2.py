bec9ebf7b8476b20ccc8581e42808e768f9601f2
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-11 05:48:22 +0200

    Update reverse_strings_2.py

--- Python Advanced/3. Lists as stacks and Queues/reverse_strings_2.py
+++ Python Advanced/3. Lists as stacks and Queues/reverse_strings_2.py
@@ -3,6 +3,7 @@ user_input = list(input())
 new_text = []
 
 for element in range(len(user_input)):
-    new_text.append(user_input.pop())
+    new_letter = user_input.pop()
+    new_text.append(new_letter)
 
 print(''.join(new_text))
