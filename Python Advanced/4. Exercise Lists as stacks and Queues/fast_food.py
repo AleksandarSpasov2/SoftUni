b815a532f36f373d598addd82c07388dc1f24e38
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-13 08:00:29 +0200

    Update fast_food.py

--- Python Advanced/4. Exercise Lists as stacks and Queues/fast_food.py
+++ Python Advanced/4. Exercise Lists as stacks and Queues/fast_food.py
@@ -1,2 +1,18 @@
+from collections import deque
+
 quantity_of_food = int(input())
-sequence = input().split()
+sequence = deque(input().split())
+
+print(max(sequence))
+
+while sequence:
+    order = sequence.popleft()
+    if int(order) >= quantity_of_food:
+        quantity_of_food -= int(order)
+    else:
+        break
+if not sequence:
+    print(f"Orders complete")
+else:
+    print(f'"Orders left: {" ".join(sequence)}')
+
