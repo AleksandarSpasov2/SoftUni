15577fe9505662ace8e99cda8f630e19191f67b0
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-14 17:33:27 +0200

    Update stacked_queues_3.py

--- Python Advanced/4. Exercise Lists as stacks and Queues/stacked_queues_3.py
+++ Python Advanced/4. Exercise Lists as stacks and Queues/stacked_queues_3.py
@@ -6,3 +6,11 @@ map_functions = {
     '3': lambda x: max(numbers) if numbers else None,
     '4': lambda x: min(numbers) if numbers else None,
 }
+
+for _ in range(int(input())):
+    numbers_data = input().split()
+    command = numbers_data[0]
+    map_functions[command](numbers_data)
+
+numbers.reverse()
+print(*numbers, sep=', ')
