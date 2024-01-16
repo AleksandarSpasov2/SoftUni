a1d5dbd3d3bfd30e909a6605dd94de27b6929cee
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-16 09:24:18 +0200

    Create cups_and_bottles.py

---
+++ Python Advanced/4. Exercise Lists as stacks and Queues/cups_and_bottles.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+glasses = deque([int(x) for x in input().split()])
+bottles = deque([int(x) for x in input().split()])
+
+waster_water = 0
+
+while glasses and bottles:
+    current_cup = glasses.popleft()
+    current_bottle = bottles.pop()
+
+    if current_cup <= current_bottle:
+        waster_water += current_bottle - current_cup
+
+    else:
+        glasses.append((current_cup - current_bottle))
+
+if glasses:
+    print(f'Cups: ', *glasses)
+else:
+    print(f'Bottles: ', *bottles)
+print(f'Wasted litters of water:{waster_water}')
