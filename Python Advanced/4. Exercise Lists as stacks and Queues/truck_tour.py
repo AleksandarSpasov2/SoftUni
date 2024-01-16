5aaa78f3e82d2af93993c4cef1af4cc0a6327c7a
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-16 07:37:03 +0200

    Update truck_tour.py

--- Python Advanced/4. Exercise Lists as stacks and Queues/truck_tour.py
+++ Python Advanced/4. Exercise Lists as stacks and Queues/truck_tour.py
@@ -1,1 +1,23 @@
-tank = int(input())
+from collections import deque
+
+truck_data = deque([[int(x) for x in input().split()] for _ in range(int(input()))])
+
+truck_data_copy = truck_data.copy()
+gas_in_tank = 0
+index = 0
+
+while truck_data_copy:
+    petrol, distance = truck_data_copy.popleft()
+
+    gas_in_tank += petrol
+
+    if gas_in_tank >= distance:
+        gas_in_tank -= distance
+
+    else:
+        truck_data.rotate(-1)
+        truck_data_copy = truck_data.copy()
+        index += 1
+        gas_in_tank = 0
+
+print(index)
