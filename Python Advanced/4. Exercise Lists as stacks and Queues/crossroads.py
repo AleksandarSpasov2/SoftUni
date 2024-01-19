21d2a63dbff1c6b4ff3ae4ea6e696a622b10ae8b
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-19 06:30:03 +0200

    Update crossroads.py

--- Python Advanced/4. Exercise Lists as stacks and Queues/crossroads.py
+++ Python Advanced/4. Exercise Lists as stacks and Queues/crossroads.py
@@ -3,3 +3,27 @@ from collections import deque
 green_light_duration = int(input())
 free_window_duration = int(input())
 
+command = input()
+
+lane = deque()
+
+while command != 'END':
+    if command != 'green':
+        lane.append(command)
+    else:
+        current_green = green_light_duration
+        current_free = free_window_duration
+        current_car = lane.popleft()
+        total_duration = green_light_duration + free_window_duration
+        if len(current_car) > green_light_duration:
+            character_hit = len(current_car) - total_duration
+            print(f"A crash happened!")
+            print(f"{current_car} was hit at {character_hit - len(current_car)}.")
+            break
+        else:
+            current_green = len(current_car) - green_light_duration
+            if current_green < 0:
+                current_free = free_window_duration + current_green
+                current_green = 0
+
+    command = input()
