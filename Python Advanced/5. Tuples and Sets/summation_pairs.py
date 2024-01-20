18ff3a9b1036094e1919f0afdb71a865d6c8819b
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-20 07:13:36 +0200

    Update summation_pairs.py

--- Python Advanced/5. Tuples and Sets/summation_pairs.py
+++ Python Advanced/5. Tuples and Sets/summation_pairs.py
@@ -1,15 +1,18 @@
-sequence = [int(x) for x in input().split()]
-target_number = int(input())
+numbers = [int(x) for x in input().split()]
+target = int(input())
+
+targets = set()
+values_map = {}
 
 
-for element in range(len(sequence) + 1):
-    if sequence[element] == '':
-        continue
-    for second_element in range(element + 1, len(sequence)):
-        if sequence[second_element] == '':
-            continue
-        if sequence[element] + sequence[second_element] == target_number:
-            print(f'{sequence[element]} + {sequence[second_element]} = {target_number}')
-            sequence[element] = ''
-            sequence[second_element] = ''
-            break
+for value in numbers:
+
+    if value in targets:
+        targets.remove(value)
+        pair = values_map[value]
+        del values_map[value]
+        print(f'{pair} + {value} = {target}')
+    else:
+        resulting_number = target - value
+        targets.add(resulting_number)
+        values_map[resulting_number] = value
