760c1d1c3f237b092a66a6fbe0017c803db05b4c
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-26 14:46:03 +0200

    updates

--- Python Advanced/8. Multidimentional Lists/lists_demo.py
+++ Python Advanced/8. Multidimentional Lists/lists_demo.py
@@ -1,9 +1,15 @@
 matrix = []
 
-for j in range(0, 3):
-    sub_list = []
-    for i in range(0, 2):
-        sub_list.append(0)
-    matrix.append(sub_list)
+# for j in range(0, 3):
+#     sub_list = []
+#     for i in range(0, 2):
+#         sub_list.append(0)
+#     matrix.append(sub_list)
+#
+# print(matrix)
 
+for j in range(3):
+    matrix.append([])
+    for i in range(2):
+        matrix[j].append(0)
 print(matrix)
