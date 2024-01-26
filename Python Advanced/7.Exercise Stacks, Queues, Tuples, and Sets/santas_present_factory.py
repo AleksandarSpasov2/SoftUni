6b0dd9a22eecd77498703f2aef38b0a294f42dc8
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-01-26 08:16:33 +0200

    Update santas_present_factory.py

--- Python Advanced/7.Exercise Stacks, Queues, Tuples, and Sets/santas_present_factory.py
+++ Python Advanced/7.Exercise Stacks, Queues, Tuples, and Sets/santas_present_factory.py
@@ -1,15 +1,15 @@
 from collections import deque
 
 materials = [int(x) for x in input().split()]
-magic_level = deque([int(x) for x in input().split()])
+magic_level = deque(int(x) for x in input().split())
 
 toys_crafted = []
 
 presents= {
-    "150": 'Doll',
-    "250": 'Wooden train',
-    "300": 'Teddy bear',
-    "400": 'Bicycle',
+    150: 'Doll',
+    250: 'Wooden train',
+    300: 'Teddy bear',
+    400: 'Bicycle',
 }
 
 while materials and magic_level:
