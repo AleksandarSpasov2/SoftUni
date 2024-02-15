0eb701c5b4c333bf340683343a7ee56a17e12416
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-02-15 11:05:07 +0200

    Create team_lineup.py

---
+++ Python Advanced/22 Exam Preparation/team_lineup.py
@@ -0,0 +1,11 @@
+def team_lineup(*kwargs):
+    
+
+
+print(team_lineup(
+   ("Harry Kane", "England"),
+   ("Manuel Neuer", "Germany"),
+   ("Raheem Sterling", "England"),
+   ("Toni Kroos", "Germany"),
+   ("Cristiano Ronaldo", "Portugal"),
+   ("Thomas Muller", "Germany")))
