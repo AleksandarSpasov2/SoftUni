46bd82ed8e5999576017a492caa1f06465aa65b4
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-02-17 05:20:59 +0200

    Update team_lineup.py

--- Python Advanced/22 Exam Preparation/team_lineup.py
+++ Python Advanced/22 Exam Preparation/team_lineup.py
@@ -1,14 +1,31 @@
 def team_lineup(*args):
-   players = {}
-   for player_name, country in args:
-      
-    
+    players = {}
+    for player_name, country in args:
+        if country not in players:
+            players[country] = []
+        players[country].append(player_name)
+
+    sorted_players = sorted(players.items(), key=lambda x: (len(x[1]), x[0]), reverse=True)
+
+    result = ''
+    for country, players in sorted_players:
+        result += f'{country}:\n'
+        for player in players:
+            result += f'  -{player}\n'
+
+    return result
 
 
 print(team_lineup(
-   ("Harry Kane", "England"),
-   ("Manuel Neuer", "Germany"),
-   ("Raheem Sterling", "England"),
-   ("Toni Kroos", "Germany"),
+
+   ("Lionel Messi", "Argentina"),
+
+   ("Neymar", "Brazil"),
+
    ("Cristiano Ronaldo", "Portugal"),
-   ("Thomas Muller", "Germany")))
+
+   ("Harry Kane", "England"),
+
+   ("Kylian Mbappe", "France"),
+
+   ("Raheem Sterling", "England")))
