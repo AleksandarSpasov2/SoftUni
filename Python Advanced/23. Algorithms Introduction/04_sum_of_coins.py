a94911ebd66337def5e5d7b4c1b78b6a4034304c
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-02-26 12:39:57 +0200

    Create 04_sum_of_coins.py

---
+++ Python Advanced/23. Algorithms Introduction/04_sum_of_coins.py
@@ -0,0 +1,10 @@
+def greedy_algo(coins, desired_sum):
+    coins_used = {}
+    coins.sort(reverse=True)
+
+    while coins > 0 and 
+
+
+
+coins = [int(x) for x in input().split(', ')]
+desired_sum = int(input())
